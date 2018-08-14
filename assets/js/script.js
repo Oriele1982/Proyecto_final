7e3f55b7b98142a7081d107b7d5a196e5fdb114b
Oriele1982 <oriele.no@hotmail.com>
2018-08-14 18:06:54 -0300

    maquetación hasta curriculum

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-        		$('.html').animate({width:'90%'},2000);
-        		$('.css').animate({width:'75%'},2000);
-        		$('.js').animate({width:'50%'},2000);
-        		$('.php').animate({width:'70%'},2000);
-        		$('.ajax').animate({width:'45%'},2000);
+        		$('.photoshop').animate({width:'85%'},2000);
+        		$('.ilustrator').animate({width:'90%'},2000);
+        		$('.indesign').animate({width:'65%'},2000);
+        		$('.html').animate({width:'85%'},2000);
+        		$('.css').animate({width:'85%'},2000);
+        		$('.js').animate({width:'60%'},2000);
+        		$('.jquery').animate({width:'65%'},2000);
+        		$('.php').animate({width:'50%'},2000);
+        		$('.word').animate({width:'85%'},2000);
+        		$('.powerpoint').animate({width:'90%'},2000);
+        		$('.excel').animate({width:'40%'},2000);
         	});
